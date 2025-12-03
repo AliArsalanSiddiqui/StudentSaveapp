fc5ec82e55b9d5e8e76aa4f4bf5ffd66c9a40aeb
Ali <arsalansiddiqui1123@gmail.com>
2025-12-03 16:50:08 +0500

    fixes

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for TurboModules
+config.resolver.sourceExts.push('mjs');
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = config;
